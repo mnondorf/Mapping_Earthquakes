75391c35b5fa068988c80c9a8b780a50cb9fc3db
Melissa <melisochka926@gmail.com>
2022-03-27 13:19:18 -0500

    13.5.6 Module Update

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -22,31 +22,32 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
-  layers: [satelliteStreets]
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/mnondorf/Mapping_Earthquakes/Mapping_Single_Points/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/mnondorf/Mapping_Earthquakes/Mapping_Single_Points/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
 }
 
 // Grabbing our GeoJSON data
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
+
 
   //Creating a GeoJSON layer with the retrieved data
   L.geoJSON(data, {
-   style: myStyle,
-    onEachFeature: function(feature, layer){
-      layer.bindPopup("<h3< Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination; " + feature.properties.dst + "</h3>");
-    }
-  }).addTo(map);
-});
-
+    style: myStyle,
+     onEachFeature: function(feature, layer){
+       layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+     }
+   }).addTo(map);
+ });
