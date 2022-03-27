69cca6fe25a833205ed64769bfbc4eb481b53dbb
Melissa <melisochka926@gmail.com>
2022-03-27 14:38:34 -0500

    13.6.3 Module Update

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -28,16 +28,6 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/mnondorf/Mapping_Earthquakes/Mapping_Single_Points/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-
-// Create a style for the lines.
-let myStyle = {
-  color: "blue",
-  fillColor: "yellow",
-  weight: 1
-}
-
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
